96817185653661b66ade7030c36d99496fda40aa
JankowiakAdam <jankowiakadam0902@gmail.com>
2022-12-04 19:38:17 +0100

    Change type of data attribute to datatime and create 3 new column based on it

--- DB_reader.py
+++ DB_reader.py
@@ -7,6 +7,4 @@ def read_data(DB_path):
     engine = create_engine(DB_path)
     connection = engine.connect()
     df = pd.read_sql("""SELECT * FROM public."DBWeather" """, con=connection)
-    # my_query = 'SELECT * FROM public."DBWeather"'
-    # results = connection.execute(my_query).fetchall()
-    print(df)
+    return df
